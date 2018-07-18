41d5de039f39a3f5456e77dc6cb76be8a9a7cbd5
sfallmann <jorundr@hotmail.com>
2018-07-18 16:27:56 -0400

    Added code provided for this assignment

--- ChallengeStudentCourses/Properties/AssemblyInfo.cs
+++ ChallengeStudentCourses/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StudentCourses")]
+[assembly: AssemblyTitle("ChallengeStudentCourses")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StudentCourses")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("ChallengeStudentCourses")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e3f22713-4f6e-485e-9599-0de6372049e1")]
+[assembly: Guid("adc82439-722b-4c68-aef2-b647005e5643")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ README.md
@@ -0,0 +1,5 @@
+# Student Courses Challenge
+
+## Requirements
+Code was provided with this challenge. Requirements were  
+interspersed within comments.
